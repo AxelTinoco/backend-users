abac0e1bf2b0d3b32de770ebbf48c9fed3da1666
Axel Tinoco <axeltm8@gmail.com>
2023-12-17 21:33:25 -0600

    middleware , routes and conect db mongodb

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  mongoURI: 'mongodb://localhost:27017/name_BD',
+  secretKey: 'jsonwebtoken'
+};
---
+++ src/server.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import connectDB from "./db.js";
+
+// Conectarse a la base de datos de MongoDB
+connectDB();
+
+// Iniciar el servidor
+app.listen(3000, () => {
+  console.log("Servidor iniciado en el puerto 3000");
+});
